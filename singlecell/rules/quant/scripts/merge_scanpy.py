d5ac37634915c4f230155a207267acedabe8f52d
flatberg <arnar.flatberg@ntnu.no>
2021-12-09 14:19:14 +0100

    add nuclear fraction to anndata

--- singlecell/rules/quant/scripts/merge_scanpy.py
+++ singlecell/rules/quant/scripts/merge_scanpy.py
@@ -1,5 +1,10 @@
+"""merge scanpy objects
+
+
+"""
 import sys
 
+import numpy as np
 import scanpy as sc
 import scvelo as scv
 
@@ -7,5 +12,14 @@ A = sc.read(sys.argv[1])
 B = sc.read(sys.argv[2])
 
 merged = scv.utils.merge(A, B, copy=True)
+
+# add nuclear fraction to obs data
+exon_sum = merged.layers['spliced'].sum(axis=1)
+intron_sum = merged.layers['unspliced'].sum(axis=1)
+nuclear_fraction = intron_sum/(exon_sum + intron_sum)
+if hasattr(nuclear_fraction, "A1"):
+    nuclear_fraction = nuclear_fraction.A1
+merged.obs['nuclear_fraction'] = nuclear_fraction
+
 
 merged.write(sys.argv[3])
