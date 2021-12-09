40f4dbdc61e36b8b09c86fd71db894122bce1589
flatberg <arnar.flatberg@ntnu.no>
2021-12-09 14:26:58 +0100

    updated umap figure with nuclear fraction and percent mitochondrial

--- singlecell/rules/bfq/scripts/plotpca.py
+++ singlecell/rules/bfq/scripts/plotpca.py
@@ -156,10 +156,10 @@ if __name__ == "__main__":
         F = F.loc[E.obs.index, :]
 
     if args.output.endswith('_mqc.png'):
-        pca_color = ['louvain', 'sample_id']
-        if 'Sample_Group' in adata.obs.columns:
-            pca_color.append('Sample_Group')
-        fig = sc.pl.umap(adata, return_fig=True, color=pca_color, ncols=1)
+        pca_color = ['louvain', 'sample_id', 'nuclear_fraction', 'fraction_mito']
+        #if 'Sample_Group' in adata.obs.columns:
+        #    pca_color.append('Sample_Group')
+        fig = sc.pl.umap(adata, return_fig=True, color=pca_color, ncols=2)
         fig.savefig(args.output, dpi=300, bbox_inches='tight')
     elif args.output.endswith('_mqc.yaml'):
         T = pd.DataFrame(adata.obsm['X_umap'], index=adata.obs_names)
